48cfb350a215544411ee5fac3ad16bd1a825cbb9
martabajo <martabg2492@gmail.com>
2014-12-12 13:40:20 +0100

    Commit 5: añadido el metodo alimentoMasCalorico, para que nos imprima por pantalla un mensaje

--- Alimento.java
+++ Alimento.java
@@ -19,7 +19,6 @@ public class Alimento
     public float calorias;
     // variable para los componenentes
     public String componente;
-    
 
     /**
      * Creado el constructor de la clase alimento.
@@ -30,7 +29,7 @@ public class Alimento
         gramosProteinas = 0;
         carbohidratos = 0;
         grasas = 0;
-        
+
     }
 
     /**
@@ -45,38 +44,46 @@ public class Alimento
         System.out.println("Calorias: " + calorias);
         System.out.println("Componente/s mayoritario/s: " + componente);
     }
-   
-   
+
     /**
      * Devuelve al valor de los Gramos por proteinas.
      */
-   public float gramosProteinas()
-   {
-    return gramosProteinas;
-   }
-   
-   /**
-    * Devuelve el valor de los carbohidratos.
-    */
-   public float carbohidratos()
-   {
-    return carbohidratos;
-   }
-  
-   /**
-    * devuelve el valor de las grasas.
-    */
-   public float grasas()
-   {
-    return grasas;
-   }
-  
-   /**
-    * Devuelve el valor de las calorias.
-    */
-   public float calorias()
-   {
-    return calorias;
-   }
+    public float gramosProteinas()
+    {
+        return gramosProteinas;
+    }
+
+    /**
+     * Devuelve el valor de los carbohidratos.
+     */
+    public float carbohidratos()
+    {
+        return carbohidratos;
+    }
+
+    /**
+     * devuelve el valor de las grasas.
+     */
+    public float grasas()
+    {
+        return grasas;
+    }
+
+    /**
+     * Devuelve el valor de las calorias.
+     */
+    public float calorias()
+    {
+        return calorias;
+    }
+
+    /**
+     * 
+     */
+    public void alimentoMasCalorico()
+    {
+        System.out.println("El alimento mas calorico ingerido por el usuario hasta el momento es: " + nombreAlimento);
+       
+    }
 
 }
--- Usuario.java
+++ Usuario.java
@@ -9,7 +9,9 @@ public class Usuario
 {
     // Atributo que da nombre al usuario
     private String nombreUsuario;
+    // Atributo de la clase alimento que da nombre al alimento.
     private Alimento nombreAlimento;
+    //Atributo de la clase alimento que coge el valor de las grasas.
     private Alimento grasas;
 
     public Usuario (String nombreUsuario, Alimento newNombreAlimento, Alimento newGrasas)
@@ -24,9 +26,13 @@ public class Usuario
         return nombreUsuario;
     }
 
+    /**
+     * Creado metodo para comer el usuario. 
+     */
     public void comer(Alimento nombreAlimento, Alimento grasas)
     {
         this.nombreAlimento = nombreAlimento;
         this.grasas = grasas;
     }
+
 }
